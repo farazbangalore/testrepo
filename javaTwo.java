f415c489efcab481c8afd3bc334a40bf3b71a4b9
Faraz Hussain <farazhussain5554@gmail.com>
2023-05-13 13:53:02 +0530

    Create javaTwo.java

---
+++ javaTwo.java
@@ -0,0 +1,24 @@
+/**
+ * The Infer "Hello World" Java example.
+ *
+ * Click the "Analyze" button to run Infer.
+ * Learn more about Infer at http://fbinfer.com
+ *
+ */
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Resources {
+
+  public static FileOutputStream allocateResource() {
+    try {
+      File file = new File("foo.txt");
+      return new FileOutputStream(file);
+    } catch (IOException e) {
+      return null;
+    }
+  }
+
+}
